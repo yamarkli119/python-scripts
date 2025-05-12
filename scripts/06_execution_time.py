ad81a4e1e1060abd4eeed7d471bf41e31ae09184
yamarkli119 <marknemo88@gmail.com>
2025-05-12 14:14:11 +0300

    Update 06_execution_time.py

--- scripts/06_execution_time.py
+++ scripts/06_execution_time.py
@@ -1,19 +1,11 @@
-"""
-ExecutionTime
-
-This class is used for timing execution of code.
-
-For example:
-
-    timer = ExecutionTime()
-    print 'Hello world!'
-    print 'Finished in {} seconds.'.format(timer.duration())
-
-"""
-
-
-import time
 import random
+import time
+# timer = ExecutionTime()
+sample_list = list()
+# my_list = [random.randint(1, 888898) for num in
+#            range(1, 1000000) if num % 2 == 0]
+# print('Finished in {} seconds.'.format(timer.duration()))
+
 
 
 class ExecutionTime:
@@ -22,13 +14,15 @@ class ExecutionTime:
 
     def duration(self):
         return time.time() - self.start_time
-
-
-# ---- run code ---- #
-
-
+    
+    
 timer = ExecutionTime()
-sample_list = list()
-my_list = [random.randint(1, 888898) for num in
-           range(1, 1000000) if num % 2 == 0]
+
+for i in range(1, 10000000):
+    if i % 2 == 0:
+        sample_list.append(random.randint(1, 888898))
 print('Finished in {} seconds.'.format(timer.duration()))
+        
+        
+        
+
